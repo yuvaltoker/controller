f6c085907e3c5e251611497c5e621be51d901864
yuvaltoker <yuvalt2710@gmail.com>
2021-12-09 16:36:52 +0200

    adjusting the code to work with rabbitmq in more generality in terms of types of queueing

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,27 @@
-FROM python:3.8-slim-buster
+# What image do we start from
+FROM ubuntu:latest
 
-RUN pip install pika
-RUN pip install minio
-RUN pip install pymongo
-WORKDIR /app
-ADD controller.py /app 
-ADD controller_lib.py /app
-ADD simple_json.json /app
-RUN chmod +x /app/controller.py
-RUN chmod +x /app/controller_lib.py
-ADD wait /app
-RUN chmod +x /app/wait
-CMD /app/wait && ./controller.py
+###################################
+#      rabbitmq dependencies      #
+###################################
+# install python dependencies
+RUN apt update && apt upgrade -y
+RUN apt install -y python 
+RUN curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py && python get-pip.py
+RUN python -m pip install pika --upgrade
+RUN pip install --upgrade pip enum34
+
+##################################################
+#      waiting for other containers service      #
+##################################################
+## Add the wait script to the image
+ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.9.0/wait /wait
+RUN chmod +x /wait
+
+## Add your application to the docker image (commented in case of working with volume)
+ADD ./controller.py .
+RUN chmod +x controller.py
+
+
+## Launch the wait tool and then your application
+CMD /wait && ./controller.py
--- scripts/communication-lib.py
+++ scripts/communication-lib.py
@@ -1,16 +1,19 @@
 #!/usr/local/bin/python
-from pymongo import MongoClient, mongo_client
+
+# for rabbitmq use
 import pika
-from minio import Minio
+
+# for access of environment variables
+import os
+
 import logging
 import uuid
-import pymongo
-# this class will create 2 q's between the report generator and the controller ,1 for telling the report genrator to create the report pdf and the other for notifing the controller.  
-class create_result_instruction(object):
-    def __init__(self):
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='rabbitmq'))
 
+class rabbitmq_handler(object):
+    def __init__(self):
+        self.rabbitmq_host = os.getenv('RMQ_HOST')
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(self.rabbitmq_host))
         self.channel = self.connection.channel()
 
         result = self.channel.queue_declare(queue='', exclusive=True)
@@ -24,8 +27,8 @@ class create_result_instruction(object):
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             self.response = body
-# 
-    def call(self):
+
+    def send(self):
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
@@ -39,6 +42,7 @@ class create_result_instruction(object):
         while self.response is None:
             self.connection.process_data_events()
         return self.response
+
 def _setup_logger():    
         logger=logging.getLogger("controller")
         logger.addHandler(logging.StreamHandler())
