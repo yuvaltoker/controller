6932459270961ab0f2a9d6552eabe575ee874697
yuvaltoker <yuvalt2710@gmail.com>
2021-12-12 11:45:34 +0200

    creating queues by names

--- scripts/communication-lib.py
+++ scripts/communication-lib.py
@@ -11,29 +11,30 @@ import uuid
 
 class rabbitmq_handler(object):
     def __init__(self):
+        self.queue_names = ['updates', 'results', 'pdfs']
+
         self.rabbitmq_host = os.getenv('RMQ_HOST')
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(self.rabbitmq_host))
         self.channel = self.connection.channel()
 
-        result = self.channel.queue_declare(queue='', exclusive=True)
-        self.callback_queue = result.method.queue
-
-        self.channel.basic_consume(
-            queue=self.callback_queue,
-            on_message_callback=self.on_response,
-            auto_ack=True)
+        # declaring the queues
+        for queue_name in self.queue_names:
+            result = self.channel.queue_declare(queue=queue_name, exclusive=True)
+            if queue_name == 'pdfs':
+                self.callback_queue = result.method.queue
+                self.channel.basic_consume(queue=self.callback_queue, on_message_callback=self.on_response_pdf, auto_ack=True)
         
-    def on_response(self, ch, method, props, body):
+    def on_response_pdf(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             self.response = body
 
-    def send(self):
+    def send(self, msg_exchange, msg_routing_key):
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
-            exchange='',
-            routing_key='controller2RGq',
+            exchange=msg_exchange,
+            routing_key=msg_routing_key,
             properties=pika.BasicProperties(
                 reply_to=self.callback_queue,
                 correlation_id=self.corr_id,
