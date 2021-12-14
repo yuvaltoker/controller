776798c3565aa3a66199c3033d289c68b8e87b37
yuvaltoker <yuvalt2710@gmail.com>
2021-12-14 16:24:13 +0200

    add a flow base to the controller.py script

--- scripts/controller.py
+++ scripts/controller.py
@@ -2,9 +2,12 @@ from pprint import PrettyPrinter
 from rabbitmq_handler import RabbitmqHandler
 from mongodb_handler import MongodbHandler
 from json import dumps, loads
-# from pymongo import MongoClient
+# for event handling (from https://stackoverflow.com/questions/6190468/how-to-trigger-function-on-value-change)
+from tkinter import *
 # for more convenient storing dictionaries
 from pandas import DataFrame
+
+rmq_handler = RabbitmqHandler()
 
 def rabbitmq_send_msg_example():
     print('im the rabbitmq example')
@@ -23,6 +26,40 @@ def mongodb_tests():
     # if collection != None:
     #     print(collection) 
     mdb_handler.print_documents(mdb_handler.get_all_documents('example_collection'))
+
+# first function to be called
+def make_test_list():
+    print('test list in proggress...')
+    print('test list ready')
+
+def test_list_ready():
+    rmq_handler.send('', 'updates', 'Test List Ready')
+
+def waiting_for_setup_ready():
+    print('im waiting for setup ready')
+
+def run_test():
+    return 'uid of test'
+
+def run_tests():
+    print('im running the tests one by one')
+    test_uid = run_test()
+    rmq_handler('', 'results', test_uid)
+
+def all_results_ready():
+    rmq_handler.send('', 'updates', 'All Results Ready')
+    link = rmq_handler.request_pdf()
+    rmq_handler.send('', 'updates', link)
+
+def controller_flow():
+    make_test_list()
+    test_list_ready()
+    waiting_for_setup_ready()
+    run_tests()
+    all_results_ready()
+
+
+
 
 if __name__ == '__main__':
     rabbitmq_send_msg_example()
