7eeb44daaee8a5c653bbbb01d86d5cea2bb24f2d
user2 <yuvalt2710@gmail.com>
2022-01-18 14:47:30 +0200

    few changes were made, the behaviour of rpc communication for the pdfs request is written well in the code

--- scripts/rabbitmq_handler.py
+++ scripts/rabbitmq_handler.py
@@ -44,7 +44,6 @@ def configure_logger_logging(logger, logging_level, logging_file):
 class RabbitmqHandler:
     def __init__(self, logging_level, logging_file = None):
         self.queue_names = os.getenv('QUEUE_NAMES').split(',')
-        #self.queue_names = ['updates', 'results', 'pdfs']
 
         self.rabbitmq_host = os.getenv('RMQ_HOST')
         self.connection = pika.BlockingConnection(
@@ -53,8 +52,10 @@ class RabbitmqHandler:
         
         # declaring the queues
         for queue_name in self.queue_names:
-            result = self.channel.queue_declare(queue=queue_name)
+            self.channel.queue_declare(queue=queue_name)
             if queue_name == 'pdfs':
+                # creating a single callback queue for the pdfs queue, in order to create the rpc behaviour
+                result = self.channel.queue_declare(queue='', exclusive=True)
                 self.callback_queue_pdfs = result.method.queue
                 self.channel.basic_consume(queue=self.callback_queue_pdfs, on_message_callback=self.on_response_pdf, auto_ack=True)
 
@@ -66,22 +67,21 @@ class RabbitmqHandler:
 
     
     def on_response_pdf(self, ch, method, props, body):
-        # check if the correlation id is the same as sent. in addition, checking if the body is not empty ('')
-        if self.corr_id == props.correlation_id and body is not '':
+        if self.corr_id == props.correlation_id:
             self.response = body
 
     # send rpc message in order to collect a 'pdf ready' notification from the report generator
     def request_pdf(self, flags):
         self.response = None
         self.corr_id = str(uuid.uuid4())
-        while self.response is None:
-            self.channel.basic_publish(
+        self.channel.basic_publish(
             exchange='',
             routing_key='pdfs',
             properties=pika.BasicProperties(
                 reply_to=self.callback_queue_pdfs,
                 correlation_id=self.corr_id),
             body='')
+        while self.response is None:
             self.connection.process_data_events()
             
             
@@ -92,7 +92,6 @@ class RabbitmqHandler:
         temp_list = flags[1]
         temp_list['pdf_link'] = self.response
         flags[1] = temp_list
-        #return self.response
 
     def send(self, msg_exchange, msg_routing_key, msg_body):
         self.channel.basic_publish(
