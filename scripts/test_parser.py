c9cfb0c0d4d8bba0b129b2930a40126aa9467b88
user2 <yuvalt2710@gmail.com>
2022-01-26 10:56:12 +0200

    parsing test is working till - creation of test by the type of test. infrastructure is improved.

--- scripts/test_parser.py
+++ scripts/test_parser.py
@@ -38,12 +38,14 @@ class TestsParser:
         # self.list_of_basic_commands includes the key words/signs which later help cutting the test into small pieces 
         self.dict_of_basic_commands = {'NAME:' : self.set_test_name, \
                                     'TEST:' : self.start_reading_test, \
-                                    '(DLEP)' : self.create_dlep_test, \
-                                    '(SNMP)' : self.create_snmp_test, \
                                     'EXPECT' : self.expect_something}
+        self.dict_of_test_type = {'(DLEP)' : self.create_dlep_test, \
+                                '(SNMP)' : self.create_snmp_test}
         self.dict_of_dlep_commands = {'SIGNAL', 'TO_INCLUDE', 'DATA_ITEM'}
         self.dict_of_snmp_commands = {'OID', 'TO_BE', 'SETTABLE', 'OF_TYPE', 'INTEGER', 'STRING', 'READONLY', 'WITH_VALUE'}
+        self.current_dict_of_commands = self.dict_of_basic_commands
         self.tests = []
+        self.test_files = []
         # logging
         logger = logging.getLogger('test_parser')
         configure_logger_logging(logger, logging_level, logging_file)
@@ -55,6 +57,10 @@ class TestsParser:
             file_lines = [line.rstrip() for line in file_lines]
         return file_lines
 
+    def parse_files(self, files):
+        for file in files:
+            self.parse_file(file)
+
     def parse_file(self, file):
         file_lines = self.file_to_lines(file)
         test_file = TestFile()
@@ -65,41 +71,46 @@ class TestsParser:
                 while len(word_list):
                     # this function can raise an exception
                     self.logger.debug(word_list)
-                    self.cut_and_parse_into_basic_variables(test_file, word_list)
+                    self.cut_and_parse_into_variables(test_file, word_list)
+            self.test_files.append(test_file)
         except CannotBeParsedError as e:
             self.logger.warning('file {} cannot be parsed. error message -> {}'.format(file, e.__str__))
 
     
-    def cut_and_parse_into_basic_variables(self, test_file, word_list):
-        if word_list[0] in self.dict_of_basic_commands:
-            self.logger.debug('inside cut_and_parse')
+    def cut_and_parse_into_variables(self, test_file, word_list):
+        if word_list[0] in self.current_dict_of_commands:
             function_to_call = word_list[0]
-            word_list[:] = word_list[1:]
             # the first word's cutting happens here, word_list[1:]
-            self.dict_of_basic_commands[function_to_call](test_file, word_list)
-            return word_list
+            word_list[:] = word_list[1:]
+            # call to function by the given key-word 
+            self.current_dict_of_commands[function_to_call](test_file, word_list)
         else:
-            raise CannotBeParsedError('cannot find a command for -> {}'.format(word_list[0]))
+            raise CannotBeParsedError('cannot find a command for -> {}'.format(' '.join(word_list[0])))
             
 
     def set_test_name(self, test_file, word_list):
-        self.logger.debug('im in set_test_name')
+        if not test_file.is_name_set:
+            test_file.set_name(' '.join(word_list))
+        else:
+            raise CannotBeParsedError('another name is given for test file {}'.format(' '.join(word_list[1:])))
         # returns empty list, cause the whole name has been read
         word_list[:] = []
 
     def start_reading_test(self, test_file, word_list):
-        self.logger.debug('im in start_reading_test')
+        # redirect the list of commands to read from dictionary of test type
+        self.current_dict_of_commands = self.dict_of_test_type
+        # there is not another cutting here, the next time cut_and_parse_into_variables will be called
+        # it will call to the right function from the self.dict_of_test_type
 
     def create_dlep_test(self, test_file, word_list):
-        self.logger.debug('im in create_dlep_test')
-
-    def create_dlep_test(self, test_file, word_list):
-        self.logger.debug('im in create_dlep_test')
+        test_file.create_dlep_test(word_list)
 
     def create_snmp_test(self, test_file, word_list):
-        self.logger.debug('im in create_snmp_test')
+        test_file.create_snmp_test(word_list)
+        
 
     def expect_something(self, test_file, word_list):
         self.logger.debug('im in expect_something')
+
 
 
--- scripts/tests.py
+++ scripts/tests.py
@@ -7,17 +7,70 @@ class TestFile:
         self.snmp_tests = []
         self.name = ''
         self.is_name_set = False
+        self.current_test = None
 
     def set_name(self, name):
         self.name = name
         self.is_name_set = True
 
-class DlepTest:
-    def __init__(self):
-        print('im dlep test')
+    def create_dlep_test(self, test_word_list):
+        self.current_test = DlepTest(test_word_list)
 
+    def create_snmp_test(self, test_word_list):
+        self.current_test = SnmpTest(test_word_list)
+
+    def delete_test(self, test):
+        if test in self.dlep_tests:
+            self.dlep_tests.remove(test)
+        elif test in self.snmp_tests:
+            self.snmp_tests.remove(test)
+
+
+class DlepTest:
+    def __init__(self, test_word_list):
+        self.test_word_list = test_word_list
+        self.signal = ''
+        self.command = ''
+        self.data_item = ''
+        self.full_test = ''
+
+    def set_signal(self, signal):
+        self.signal = signal
+
+    def set_command(self, command):
+        self.command = command
+
+    def set_data_item(self, data_item):
+        self.data_item = data_item
+
+    def build_test(self):
+        self.full_test = '{}, {}, {}'.format(self.signal, self.command, self.data_item)
+
+    def get_test(self):
+        return self.full_test
+    
 
 
 class SnmpTest:
-    def __init__(self):
-        print('im snmp test')
+    def __init__(self, test_word_list):
+        self.test_word_list = test_word_list
+        self.oid = ''
+        # command can be set/get
+        self.command = ''
+        self.value = ''
+        self.full_test = ''
+
+    def set_command(self, command):
+        self.command = command
+
+    def set_oid(self, oid):
+        self.oid = oid
+
+    def set_value(self, value):
+        self.value = value
+
+    def build_test(self):
+        self.full_test = '{}, {}, {}'.format(self.oid, self.command, self.value)
+
+    def get_test(self):
+        return self.full_test
